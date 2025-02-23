3f9d98c7c7a67f30da2b15f192811526a97383e6
JDHA15 <josue.hernandez2026@hilasal.superate.org.sv>
2025-02-23 14:42:31 -0600

    Mejoras en la tabla y condicional

--- calculador_IMC.py
+++ calculador_IMC.py
@@ -5,9 +5,10 @@ tabla = [] #para guardar los IMC calculados anteriormente
 
 while True:
     print('Bienvenido a tu calculadora de IMC\n')#menú interactivo
-    print('1. Iniciar a calcular tu IMC\n')
-    print('2. Accede a tu tabla de datos\n')
-    print('3. Salir del programa\n')
+    print('El índice de masa corporal (IMC) es un método utiizado para estimar la cantidad de grasa corporal que tienen una persona, y determianr por tanto si el peso está dentro del rango normal, o po el contrario, si tiene sobreperso o delgadez. Con esta calculadora podras ver si tu IMC esta dentro de lo recomendado, y si no es así reciviras recomendaciones para llegar a tu IMC recomendado.\n') 
+    print('Opción 1. Iniciar a calcular tu IMC\n')
+    print('Opción 2. Accede a tu tabla de datos\n')
+    print('Opción 3. Salir del programa\n')
 
     
     opcion = int(input('Selecciona tu opción: '))#seleccion de opciones
@@ -19,32 +20,66 @@ while True:
         peso = float(input('Ingresa tu peso en kilogramos: \n'))
         altura = float(input('Ingresa tu altura en metros: \n'))
         dato3 = int(input('Ingresa tu edad: \n'))               #agregado para solicitar edad y dar consejeria
-        if dato3 >= 65:
-                        print('Tu indice de masa corporal ideal es entre 24-29, si tu indice de masa corporal es alto te recomendamos llevar una dieta balanceada, aunque no te recomendamos que inicies repentinamente, inicia poco a poco. Tambien te recomendamos hacer actividad física, tomar suficiente agua y dormir lo necesario.\n')
-                        print('Si tu indice de masa corporal es bajo te recomendamos comer mas proteínas, comer mas calorias, incluir mas carbohidratos en tu vida, pero siempre teniendo cuidado de no comer alimentos altamente procesados\n')
-        elif dato3 >= 55 and dato3 <= 64:
-                        print('Tu indice de masa corporal ideal es entre 23-28, si tu indice de masa corporal es alto te    recomendamos llevar una dieta balanceada, aunque no te recomendamos que inicies repentinamente, inicia poco a poco. Tambien te recomendamos hacer actividad física, tomar suficiente agua y dormir lo necesario.')
-                        print('Si tu indice de masa corporal es bajo te recomendamos comer mas proteínas, comer mas calorias, incluir mas carbohidratos en tu vida, pero siempre teniendo cuidado de no comer alimentos altamente procesados')
-        elif dato3 >= 45 and dato3 <= 54:
-                        print('Tu indice de masa corporal ideal es entre 22-27, si tu indice de masa corporal es alto te recomendamos llevar una dieta balanceada, aunque no te recomendamos que inicies repentinamente, inicia poco a poco. Tambien te recomendamos hacer actividad física, tomar suficiente agua y dormir lo necesario.')
-                        print('Si tu indice de masa corporal es bajo te recomendamos comer mas proteínas, comer mas calorias, incluir mas carbohidratos en tu vida, pero siempre teniendo cuidado de no comer alimentos altamente procesados')
-        elif dato3 >= 35 and dato3 <= 44:
-                        print('Tu indice de masa corporal ideal es entre 21-26, si tu indice de masa corporal es alto te recomendamos llevar una dieta balanceada, aunque no te recomendamos que inicies repentinamente, inicia poco a poco. Tambien te recomendamos hacer actividad física, tomar suficiente agua y dormir lo necesario.')
-                        print('Si tu indice de masa corporal es bajo te recomendamos comer mas proteínas, comer mas calorias, incluir mas carbohidratos en tu vida, pero siempre teniendo cuidado de no comer alimentos altamente procesados')
-        elif dato3 >= 25 and dato3 <= 34:
-                        print('Tu indice de masa corporal ideal es entre 20-25, si tu indice de masa corporal es alto te recomendamos llevar una dieta balanceada, aunque no te recomendamos que inicies repentinamente, inicia poco a poco. Tambien te recomendamos hacer actividad física, tomar suficiente agua y dormir lo necesario.')
-                        print('Si tu indice de masa corporal es bajo te recomendamos comer mas proteínas, comer mas calorias, incluir mas carbohidratos en tu vida, pero siempre teniendo cuidado de no comer alimentos altamente procesados')
-        elif dato3 >=18 and dato3 <= 24:
-                        print('Tu indice de masa corporal ideal es entre 19-24, si tu indice de masa corporal es alto te    recomendamos llevar una dieta balanceada, aunque no te recomendamos que inicies repentinamente, inicia poco a poco. Tambien te recomendamos hacer actividad física, tomar suficiente agua y dormir lo necesario.')
-                        print('Si tu indice de masa corporal es bajo te recomendamos comer mas proteínas, comer mas calorias, incluir mas carbohidratos en tu vida, pero siempre teniendo cuidado de no comer alimentos altamente procesados') 
-        elif dato3 <18:
-                        print('Tu indice de masa corporal ideal es entre 14-20, si tu indice de masa corporal es alto te    recomendamos llevar una dieta balanceada, aunque no te recomendamos que inicies repentinamente, inicia poco a poco. Tambien te recomendamos hacer actividad física, tomar suficiente agua y dormir lo necesario.')
-                        print('Si tu indice de masa corporal es bajo te recomendamos comer mas proteínas, comer mas calorias, incluir mas carbohidratos en tu vida, pero siempre teniendo cuidado de no comer alimentos altamente procesados') 
-    
+
         IMC = calcula_imc(peso, altura)
-        tabla.append(f'IMC: {IMC:.2f}')#el uso de :.2f para mandar solo dos decimales
+        tabla.append(f'tus datos: Peso {peso}, altura: {altura}, tu edad: {dato3}, tu IMC: {IMC:.2f}')#el uso de :.2f para mandar solo dos decimales
         print(f'Tu IMC es {IMC:.2f}')
-        
+        if dato3 >= 65:
+            if IMC >29:
+                print('Tu IMC es mayor que lo recomendado\n')
+                print('Tu indice de masa corporal ideal es entre 24-29, Tu indice de masa corporal es alto. Te recomendamos llevar una dieta balanceada, aunque no te recomendamos que inicies repentinamente, inicia poco a poco. Tambien te recomendamos hacer actividad física, tomar suficiente agua y dormir lo necesario.\n')
+            elif IMC <24:
+                print(' Tu indice de masa corporal ideal es entre 24-29. tu indice de masa corporal es bajo. Te recomendamos comer mas proteínas, comer mas calorias, incluir mas carbohidratos en tu vida, pero siempre teniendo cuidado de no comer alimentos altamente procesados\n')
+            elif IMC >=24 or IMC <=29:
+                print('Tu IMC es normal')   
+        elif dato3 >= 55 and dato3 <= 64:
+            if IMC >28:
+                print('Tu IMC es mayor que lo recomendado\n')
+                print('Tu indice de masa corporal ideal es entre 23-28, Tu indice de masa corporal es alto. Te recomendamos llevar una dieta balanceada, aunque no te recomendamos que inicies repentinamente, inicia poco a poco. Tambien te recomendamos hacer actividad física, tomar suficiente agua y dormir lo necesario.\n')
+            elif IMC <23:
+                print(' Tu indice de masa corporal ideal es entre 23-28. tu indice de masa corporal es bajo. Te recomendamos comer mas proteínas, comer mas calorias, incluir mas carbohidratos en tu vida, pero siempre teniendo cuidado de no comer alimentos altamente procesados\n')
+            elif IMC >=23 or IMC <=28:
+                print('Tu IMC es normal') 
+        elif dato3 >= 45 and dato3 <= 54:
+            if IMC >27:
+                print('Tu IMC es mayor que lo recomendado\n')
+                print('Tu indice de masa corporal ideal es entre 22-27, Tu indice de masa corporal es alto. Te recomendamos llevar una dieta balanceada, aunque no te recomendamos que inicies repentinamente, inicia poco a poco. Tambien te recomendamos hacer actividad física, tomar suficiente agua y dormir lo necesario.\n')
+            elif IMC <22:
+                print(' Tu indice de masa corporal ideal es entre 22-27. tu indice de masa corporal es bajo. Te recomendamos comer mas proteínas, comer mas calorias, incluir mas carbohidratos en tu vida, pero siempre teniendo cuidado de no comer alimentos altamente procesados\n')
+            elif IMC >=22 or IMC <=27:
+                print('Tu IMC es normal') 
+        elif dato3 >= 35 and dato3 <= 44:
+            if IMC >26:
+                print('Tu IMC es mayor que lo recomendado\n')
+                print('Tu indice de masa corporal ideal es entre 21-26, Tu indice de masa corporal es alto. Te recomendamos llevar una dieta balanceada, aunque no te recomendamos que inicies repentinamente, inicia poco a poco. Tambien te recomendamos hacer actividad física, tomar suficiente agua y dormir lo necesario.\n')
+            elif IMC <21:
+                print(' Tu indice de masa corporal ideal es entre 21-26. tu indice de masa corporal es bajo. Te recomendamos comer mas proteínas, comer mas calorias, incluir mas carbohidratos en tu vida, pero siempre teniendo cuidado de no comer alimentos altamente procesados\n')
+            elif IMC >=21 or IMC <=26:
+                print('Tu IMC es normal')
+        elif dato3 >= 25 and dato3 <= 34:
+            if IMC >25:
+                print('Tu IMC es mayor que lo recomendado\n')
+                print('Tu indice de masa corporal ideal es entre 20-25, Tu indice de masa corporal es alto. Te recomendamos llevar una dieta balanceada, aunque no te recomendamos que inicies repentinamente, inicia poco a poco. Tambien te recomendamos hacer actividad física, tomar suficiente agua y dormir lo necesario.\n')
+            elif IMC <20:
+                print(' Tu indice de masa corporal ideal es entre 20-25. tu indice de masa corporal es bajo. Te recomendamos comer mas proteínas, comer mas calorias, incluir mas carbohidratos en tu vida, pero siempre teniendo cuidado de no comer alimentos altamente procesados\n')
+            elif IMC >=20 or IMC <=25:
+                print('Tu IMC es normal') 
+        elif dato3 >=18 and dato3 <= 24:
+            if IMC >24:
+                print('Tu IMC es mayor que lo recomendado\n')
+                print('Tu indice de masa corporal ideal es entre 19-24, Tu indice de masa corporal es alto. Te recomendamos llevar una dieta balanceada, aunque no te recomendamos que inicies repentinamente, inicia poco a poco. Tambien te recomendamos hacer actividad física, tomar suficiente agua y dormir lo necesario.\n')
+            elif IMC <19:
+                print(' Tu indice de masa corporal ideal es entre 19-24. tu indice de masa corporal es bajo. Te recomendamos comer mas proteínas, comer mas calorias, incluir mas carbohidratos en tu vida, pero siempre teniendo cuidado de no comer alimentos altamente procesados\n')
+            elif IMC >=19 or IMC <=24:
+                print('Tu IMC es normal')  
+        elif dato3 <18:
+            if IMC >20:
+                print('Tu IMC es mayor que lo recomendado\n')
+                print('Tu indice de masa corporal ideal es entre 14-20, Tu indice de masa corporal es alto. Te recomendamos llevar una dieta balanceada, aunque no te recomendamos que inicies repentinamente, inicia poco a poco. Tambien te recomendamos hacer actividad física, tomar suficiente agua y dormir lo necesario.\n')
+            elif IMC <14:
+                print(' Tu indice de masa corporal ideal es entre 14-20. tu indice de masa corporal es bajo. Te recomendamos comer mas proteínas, comer mas calorias, incluir mas carbohidratos en tu vida, pero siempre teniendo cuidado de no comer alimentos altamente procesados\n')
+            elif IMC >=14 or IMC <=20:
+                print('Tu IMC es normal')
     elif opcion == 2:
         if tabla:
             print('Tabla de datos:')
